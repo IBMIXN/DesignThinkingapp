4cb9708cc3ab442ebd262abf2a402957dd40c887
wangyh46 <yuhengw46@gmail.com>
2021-05-30 23:15:33 +0100

    Finalise

--- designthinking/audio.py
+++ designthinking/audio.py
@@ -1,6 +1,6 @@
 from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import simpleaudio as sa
+# import simpleaudio as sa
 
 apikey = "FnR0IEGhCkL_t20v1yphYJruUSgIatOmeSo_ba7WDk8u"
 url = "https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/6f715e03-1d19-4bb9-80a9-f5c751abf555"
@@ -16,11 +16,11 @@ def testGetVoice():
 
 def speak(text):
     print(text)
-    with open('./speech.wav', 'wb') as audio_file:
-        res = tts.synthesize(text, accept='audio/wav', voice=voiceModel).get_result()
-        audio_file.write(res.content)
+    # with open('./speech.wav', 'wb') as audio_file:
+    #     res = tts.synthesize(text, accept='audio/wav', voice=voiceModel).get_result()
+    #     audio_file.write(res.content)
 
-    filename = 'speech.wav'
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done() 
+    # filename = 'speech.wav'
+    # wave_obj = sa.WaveObject.from_wave_file(filename)
+    # play_obj = wave_obj.play()
+    # play_obj.wait_done() 
